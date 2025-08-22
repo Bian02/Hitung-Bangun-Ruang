9d316df3f11cd7c42df6d42275d4f9ebf9401efa
Pebrian Setiawan <137418760+Bian02@users.noreply.github.com>
2025-08-22 22:22:29 +0800

    Update main.js
    Tambahan kode yang dapat menampilkan segitiga ke halaman

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+document.getElementById("segitiga").addEventListener("click", () => {
+    const segitiga = 
+        `<p class="pertanyaan">Mencari Panjang Sisi Miring "ac"</p>
+        <section class="tinggi">
+            <p id="a">a</p>
+            <input type="number" name="Tinggi" id="inputTinggi" required />
+            <p id="pesan"></p>
+            <section class="bangunRuang">
+                <svg height="300px" width="400px" class="segiTigaSiku-Siku">
+                    <path fill="lightgray" stroke="black"
+                    d="M 0,20 L 0,240 L 400,240 L 0,20"/>
+                </svg>
+                <p id="tandaTanya">?</p>
+                <p id="b">b</p>
+                <p id="c">c</p>
+            <input type="number" name="Alas" id="inputAlas" required/>
+        </section>
+        <button type="submit" id="tombol">Submit</button>`
+    document.querySelector(".isi").innerHTML = segitiga;
+});
+
+
 document.getElementById("tombol").addEventListener("click", () => {
     const a = document.getElementById("inputTinggi").value;
     const b = document.getElementById("inputAlas").value;
@@ -16,7 +38,7 @@ document.getElementById("tombol").addEventListener("click", () => {
     const kalimat5 = `<p>c² = ${a}² + ${b}²</p>`
     const kalimat6 = `<p>c² = ${a**2} + ${b**2}</p>`;
     const kalimat7 = `<p>c² = ${a**2 + b**2}</p>`;
-    const kalimat8 = `<p>c = ✓${a**2 + b**2}</p>`;
+    const kalimat8 = `<p>c = √${a**2 + b**2}</p>`;
     const kalimat9 = `<p>c = ${c}</p>`;
     const kalimat10 = `<p>Jadi, panjang sisi miring "ac" adalah ${c}</p>`
     const hasil = kalimat1 + kalimat2 + kalimat3 + kalimat4 + kalimat5 + kalimat6 + kalimat7 + kalimat8 + kalimat9 + kalimat10;
